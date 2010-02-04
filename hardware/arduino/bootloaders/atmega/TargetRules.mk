c85114d894110e54ec8e6c6fce568b0e7eff7900
giuliano carlini <giuliano@carlini.com>
2010-02-04 15:25:22 -0800

    - More makefile cleanups

--- hardware/arduino/bootloaders/atmega/TargetRules.mk
+++ hardware/arduino/bootloaders/atmega/TargetRules.mk
@@ -1,8 +1,11 @@
 HEX      := $(PROGRAM)_$(TARGET).hex
+ELF      := build/$(PROGRAM)_$(TARGET).elf
+ISP      := $(TARGET)_isp
 
 PROG_OBJ := build/$(PROGRAM)_$(TARGET).o
 SPM_OBJ  := build/spm_$(TARGET).o
-OBJS     := $(PROG_OBJ) $(SPM_OBJ)
+
+OBJS     := $(SOURCES:%.c=build/%_$(TARGET).o)
 
 OBJ_DEPS := $(OBJS:.o=.d)
 
@@ -18,19 +21,9 @@ $(ELF): $(PROG_OBJ) $(SPM_OBJ)
 
 $(PROG_OBJ): $(MAKEFILE_LIST)
 $(PROG_OBJ): $(PROG_S)
-$(PROG_OBJ): $(PROGRAM).c
-	$(COMPILE_CMD)
 
 $(SPM_OBJ): $(MAKEFILE_LIST)
 $(SPM_OBJ): $(SPM_S)
-$(SPM_OBJ): spm.c
-	$(COMPILE_CMD)
-
-$(PROG_S): $(PROGRAM).c $(MAKEFILE_LIST)
-	$(AS_CMD)
-
-$(SPM_S): spm.c $(MAKEFILE_LIST)
-	$(AS_CMD)
 
 $(TARGET): TARGET := $(TARGET)
 $(TARGET): $(HEX)
@@ -38,6 +31,13 @@ $(TARGET): $(HEX)
 $(ISP): $(TARGET)
 $(ISP): TARGET := $(TARGET)
 $(ISP): isp
+
+build/%_$(TARGET).o: build/%_$(TARGET).s
+build/%_$(TARGET).o: %.c
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+build/%_$(TARGET).s: %.c
+	$(AS_CMD)
 
 PROG_DEP = $(PROG_OBJ:.o=.d)
 $(PROG_DEP): $(PROGRAM).c $(MAKEFILE_LIST)
@@ -53,4 +53,6 @@ $(SPM_DEP): spm.c $(MAKEFILE_LIST)
 	sed 's,.*\.o[ :]*,$(SPM_OBJ) $@ : ,g' < $@.$$$$ > $@; \
 	rm -f $@.$$$$
 
-include $(OBJ_DEPS)
+ifneq ($(MAKECMDGOALS),clean)
+    include $(OBJ_DEPS)
+endif
