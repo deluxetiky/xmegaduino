869dd35620a0f832885e3abf39446be0de3c2593
giuliano carlini <giuliano@carlini.com>
2010-03-15 00:19:33 -0700

    - Kill debug code.

--- app/src/processing/app/debug/Uploader.java
+++ app/src/processing/app/debug/Uploader.java
@@ -125,10 +125,6 @@ public abstract class Uploader implements MessageConsumer  {
         }
         System.out.println();
       }
-System.out.println("Execute command");
-for ( String str : commandArray ) {
- System.out.println(str);
-}
       Process process = Runtime.getRuntime().exec(commandArray);
       new MessageSiphon(process.getInputStream(), this);
       new MessageSiphon(process.getErrorStream(), this);
