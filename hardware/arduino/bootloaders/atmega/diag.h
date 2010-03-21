a5de7b1752be5544cd6cf8562fa1d6f579efe623
GorillaCoder <giuliano@carlini.com>
2010-03-20 17:51:01 -0700

    - Blink LED for selected USART. - Add diagnostics.

--- hardware/arduino/bootloaders/atmega/diag.h
+++ hardware/arduino/bootloaders/atmega/diag.h
@@ -6,17 +6,22 @@
 */
 
 #if !defined DIAG_ENABLE
-    #define DIAG_ENABLE 0
+    #define DIAG_ENABLE 1
 #endif
 
 // TODO: Move to diag.h
 #if 1 <= DIAG_ENABLE
-    static void DiagEnable(uint8_t bootuart);
-    static void DiagChar(char value);
-    static void Diag(const char *value);
-    static void DiagNumber(unsigned long value, uint8_t base);
+    void DiagEnable(uint8_t bootuart);
+    void DiagChar(char value);
+    void Diag(const char *value);
+    void DiagNumber(unsigned long value, uint8_t base);
+	void DiagStrNumber( const char* str, unsigned long n, uint8_t base, int nl );
 #else
     #define DiagEnable(bootuart)
+	#define DiagChar(value)
+	#define Diag(value)
+	#define DiagNumber(value, base)
+	#define DiagStrNumber(str, number, base, nl)
 #endif
 
 
